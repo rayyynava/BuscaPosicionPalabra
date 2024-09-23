29e2230e884f00a181449053274d355616864841
rayyynava <119821148+rayyynava@users.noreply.github.com>
2024-09-23 13:23:14 -0600

    BuscaPalabraPosicion.cpp

--- BuscaPalabra.cpp
+++ BuscaPalabra.cpp
@@ -1,1 +1,36 @@
-Este proyecto fue desarrollado con el propósito de permitir al usuario buscar una palabra en un texto, identificando en qué posición comienza y en qué posición termina dicha palabra.
+#include <iostream>
+#include <cstring>  // Incluye esta cabecera para usar strlen
+
+using namespace std;
+
+int main()
+{
+    char texto[30];
+    char palabra[30];
+    int inicio;
+    int fin;
+
+    cout << "Ingrese texto: ";
+    cin.getline(texto, 30, '\n');
+
+    cout << "Ingrese palabra a buscar: ";
+    cin.getline(palabra, 30, '\n');
+
+    int j = 0;
+    bool existeP = false;
+
+    while (existeP == false) {
+        for (int k = 0; k < strlen(palabra); k++) {
+            if (texto[j] == palabra[k]) {
+                fin = j;
+            } else {
+                k = 0;
+            }
+            j++;
+        }
+        existeP = true;
+    }
+
+    inicio = fin - strlen(palabra) + 1;
+    cout << "La palabra inicia en la posicion " << inicio << " y termina en la posicion " << fin;
+}
