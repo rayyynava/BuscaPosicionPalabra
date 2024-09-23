e8c966dc84120b62afd3d242742386b59e7dbac8
rayyynava <119821148+rayyynava@users.noreply.github.com>
2024-09-23 12:53:30 -0600

    Archivo de C++ busqueda de palabra

---
+++ BuscaPalabra.cpp
@@ -0,0 +1,1 @@
+Este proyecto fue desarrollado con el propósito de permitir al usuario buscar una palabra en un texto, identificando en qué posición comienza y en qué posición termina dicha palabra.
